409078e7ed66a3ec836a8f18ae8d0aea7f50ad8e
Marcel Mravec <marcel.mravec@cleverlance.com>
2023-09-21 19:21:41 +0200

    Initial Commit

---
+++ Lifehacks/LifehacksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LifehacksApp.swift
+//  Lifehacks
+//
+//  Created by Marcel Mravec on 21.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct LifehacksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
